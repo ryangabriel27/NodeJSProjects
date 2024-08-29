06c30d4f258abc42882634519024c3cad956e399
ryangabriel27 <ryanjulio56@gmail.com>
2024-08-29 16:55:35 -0300

    29.08

--- api-biblioteca/app.js
+++ api-biblioteca/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 const livroRoutes = require("./routes/livroRoutes");
 require("dotenv").config();
@@ -11,9 +10,7 @@ const app = express();
 // Middlewares
 // Configura o middleware CORS
 app.use(cors());
-app.use(bodyParser.json());
-
-
+app.use(express.json());
 
 // Rotas
 app.use("/livros", livroRoutes);
